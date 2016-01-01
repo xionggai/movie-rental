afaa4dfb9828e3ac3266003c86dd4964d38d11c3
xionggai <hustphysics@hotmail.com>
2016-01-01 18:11:45 +0800

    Initial Commit

---
+++ MovieRental/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MovieRental
+//
+//  Created by xionggai on 1/1/16.
+//  Copyright © 2016 xionggai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
