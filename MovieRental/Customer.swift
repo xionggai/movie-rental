532cd9e4280f972dca018417fa63f563bc2c91f9
xionggai <hustphysics@hotmail.com>
2016-01-01 18:45:30 +0800

    remove local vars with query

--- MovieRental/Customer.swift
+++ MovieRental/Customer.swift
@@ -34,21 +34,37 @@ class Customer: NSObject {
         }
     }
     
+    private var totalCharge: Double {
+        get {
+            var result: Double = 0
+            for rental in rentals {
+                result += rental.charge
+            }
+            return result
+        }
+    }
+    
+    private var totalFrequentRenterPoints: Double {
+        get {
+            var result: Double = 0
+            for rental in rentals {
+                result += rental.frequentRenterPoints
+            }
+            return result
+        }
+    }
+    
     func statement() -> String {
-        
-        var totalAmount: Double = 0
-        var frequentRenterPoints:Double = 0
-        
         var result: String = "Rental Record for \(name) \n"
         
         for rental in rentals {
             
-            frequentRenterPoints += rental.frequentRenterPoints
             result += "\t \(rental.movie.title) \t \(rental.charge) \n"
-            totalAmount += rental.charge
         }
-        result += "Amount owned is \(totalAmount) \n"
-        result += "You earned \(frequentRenterPoints) frequent renter points"
+        
+        result += "Amount owned is \(totalCharge) \n"
+        result += "You earned \(totalFrequentRenterPoints) frequent renter points"
+        
         return result
     }
 }
